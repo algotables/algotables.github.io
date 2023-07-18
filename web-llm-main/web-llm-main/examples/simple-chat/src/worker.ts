748c998bc27a75317d9fd84beb53cf53ade2f63d
algotables <mistermagnify@gmail.com>
2023-07-18 12:24:31 -0700

    Testing cont.

--- web-llm-main/web-llm-main/examples/simple-chat/src/worker.ts
+++
@@ -1,8 +0,0 @@
-// Serve the chat workload through web worker
-import { ChatWorkerHandler, ChatModule } from "@mlc-ai/web-llm";
-
-const chat = new ChatModule();
-const handler = new ChatWorkerHandler(chat);
-self.onmessage = (msg: MessageEvent) => {
-  handler.onmessage(msg);
-};
